eddfd94769ea9cd66edb0a6f65a37b04745bd5c0
Paul Fioravanti <paul.fioravanti@gmail.com>
2020-06-22 10:55:52 +1000

    Turn square into functional component

--- src/Square.js
+++ src/Square.js
@@ -1,14 +1,9 @@
 import React from "react";
 
-export class Square extends React.Component {
-  render() {
-    return (
-      <button
-        className="square"
-        onClick={this.props.onClick.bind(this)}
-      >
-        {this.props.value}
-      </button>
-    );
-  }
+export function Square(props) {
+  return (
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
+  );
 }
