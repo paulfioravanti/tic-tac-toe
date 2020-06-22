129789bf22305295cd14fbbeeb2f9aa37fcc4936
Paul Fioravanti <paul.fioravanti@gmail.com>
2020-06-22 12:10:43 +1000

    Board refactor

--- src/Board.js
+++ src/Board.js
@@ -2,40 +2,86 @@ import React from "react";
 import { Square } from "./Square";
 
 export class Board extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      squares: Array(9).fill(null),
-      xIsNext: true
+  static INITIAL_BOARD = Array(9).fill(null)
+  static CELLS = Board.chunk([...Board.INITIAL_BOARD.keys()], 3)
+  static LINES = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  static chunk(array, size) {
+    const chunked = [];
+    let index = 0;
+    while (index < array.length) {
+      chunked.push(array.slice(index, size + index));
+      index += size;
     }
+    return chunked;
+  }
+
+  static calculateWinner(squares) {
+    for (let i = 0, len = Board.LINES.length; i < len; i++) {
+      const [a, b, c] = Board.LINES[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Board.INITIAL_BOARD,
+      xIsNext: true
+    };
   }
 
   render() {
-    const status = `Next player: ${this.state.xIsNext ? "X" : "O"}`;
-
     return (
       <div>
-        <div className="status">{status}</div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
+        <div className="status">
+          {this.status()}
         </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
+        {this.renderBoardRows()}
       </div>
     );
   }
 
   // PRIVATE
+
+  status() {
+    const winner = Board.calculateWinner(this.state.squares);
+    let status;
+
+    if (winner) {
+      status = `Winner: ${winner}`;
+    } else {
+      status = `Next player: ${this.state.xIsNext ? "X" : "O"}`;
+    }
+
+    return status;
+  }
+
+  renderBoardRows() {
+    return Board.CELLS.map((row) => this.renderBoardRow(row))
+  }
+
+  renderBoardRow([a, b, c]) {
+    return (
+      <div className="board-row">
+        {this.renderSquare(a)}
+        {this.renderSquare(b)}
+        {this.renderSquare(c)}
+      </div>
+    );
+  }
 
   renderSquare(i) {
     return (
@@ -48,6 +94,10 @@ export class Board extends React.Component {
 
   handleClick(i) {
     const squares = this.state.squares.slice();
+    if (squares[i] || Board.calculateWinner(squares)) {
+      return;
+    }
+
     const xIsNext = this.state.xIsNext
     squares[i] = xIsNext ? "X" : "O";
     this.setState({
