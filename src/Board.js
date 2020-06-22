675598de0690d53911474d37761f3e3066b044f8
Paul Fioravanti <paul.fioravanti@gmail.com>
2020-06-22 11:14:06 +1000

    Finish taking turns

--- src/Board.js
+++ src/Board.js
@@ -5,12 +5,13 @@ export class Board extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      squares: Array(9).fill(null)
+      squares: Array(9).fill(null),
+      xIsNext: true
     }
   }
 
   render() {
-    const status = "Next player: X";
+    const status = `Next player: ${this.state.xIsNext ? "X" : "O"}`;
 
     return (
       <div>
@@ -47,7 +48,11 @@ export class Board extends React.Component {
 
   handleClick(i) {
     const squares = this.state.squares.slice();
-    squares[i] = "X";
-    this.setState({ squares: squares });
+    const xIsNext = this.state.xIsNext
+    squares[i] = xIsNext ? "X" : "O";
+    this.setState({
+      squares: squares,
+      xIsNext: !xIsNext
+    });
   }
 }
