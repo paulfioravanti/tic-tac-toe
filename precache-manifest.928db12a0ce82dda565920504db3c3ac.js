7ceeb6174c884b234dd22e2a675c8b9b10573402
Paul Fioravanti <paul.fioravanti@gmail.com>
2020-06-22 19:50:29 +1000

    Updates

---
+++ precache-manifest.928db12a0ce82dda565920504db3c3ac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "190f2249d003fd83f3ad98213a76fc91",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "08f93981dd5f22693f07",
+    "url": "/tic-tac-toe/static/css/main.3fa7586b.chunk.css"
+  },
+  {
+    "revision": "cb66f8ce382e7ee99659",
+    "url": "/tic-tac-toe/static/js/2.6e4774a1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tic-tac-toe/static/js/2.6e4774a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "08f93981dd5f22693f07",
+    "url": "/tic-tac-toe/static/js/main.08323fc3.chunk.js"
+  },
+  {
+    "revision": "7f32745e7b69030faee7",
+    "url": "/tic-tac-toe/static/js/runtime-main.df28867d.js"
+  }
+]);
